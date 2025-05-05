cd11dba4f3549a5b78fca2d5c785aefc24983561
LAB53 <lab53@fatec.com>
2025-05-05 10:15:49 -0300

    Programa que mostra se o número é par ou ímpar

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿int numero;
+string parimpar;
+
+Console.Write("Digite um número: ");
+numero = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+
+parimpar = (numero % 2) == 0 ? "par" : "ímpar";
+Console.WriteLine($"O {numero} é {parimpar}");
